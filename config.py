99caff6c3daca8beaff098571cef9c1f99a54d9a
desacimanukcimanuk-alt <desacimanukcimanuk@gmail.com>
2026-07-28 17:55:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("MYSQLHOST")
+DB_USER = os.getenv("MYSQLUSER")
+DB_PASSWORD = os.getenv("MYSQLPASSWORD")
+DB_NAME = os.getenv("MYSQLDATABASE")
+DB_PORT = int(os.getenv("MYSQLPORT", 3306))
 
 SECRET_KEY = os.getenv("SECRET_KEY", "Ciman")
 UPLOAD_FOLDER = "static/upload"
